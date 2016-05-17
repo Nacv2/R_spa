1ef3dbcaaf77ac1de1a8314725aeab8830b9d246
Nacv2 <nikelfo@hotmail.com>
2016-05-17 02:51:28 -0500

    Create updateR.R

---
+++ updateR.R
@@ -0,0 +1,4 @@
+install.packages("installr") # Función para instalar el paquete "installr"
+library("installr") # Función para cargar el paquete "installr"
+
+updateR(update_packages = T, setInternet2 = F) # Función para actualizar R
